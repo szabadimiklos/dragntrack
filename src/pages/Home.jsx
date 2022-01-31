008b696ce03a2276659ef40e5148405926e34fdc
Szabadi Miklós <szabadimiklos95@gmail.com>
2022-01-31 20:50:22 +0100

    Apexcharts added, some elements created on the homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react"
+import { Link } from "react-router-dom"
+import Chart from 'react-apexcharts'
+import UserMenu from "../components/UserMenu"
 
-import { 
+import {
 	AppBar,
 	Box,
 	Button,
+	Card,
+	CardActions,
+	CardContent,
+	CardMedia,
 	Container,
 	Divider,
 	Fade,
+	Grid,
 	IconButton,
 	ListItemIcon,
 	ListItemText,
@@ -21,8 +29,8 @@ import {
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined'
 import ContactlessOutlinedIcon from '@mui/icons-material/ContactlessOutlined'
 import MenuIcon from '@mui/icons-material/Menu'
-import { Link } from "react-router-dom"
-import UserMenu from "../components/UserMenu"
+
+import goalimage from '../assets/images/tesla.jpg'
 
 const mainMenuList = [
 	{
@@ -36,6 +44,257 @@ const mainMenuList = [
 		to: '/'
 	},
 ]
+
+const demochart = {
+	series: [{
+		name: 'Incomes',
+		data: [3100, 4000, 3300, 5100, 4200, 4500, 5000, 4650, 5100, 5500, 4850, 6100]
+	}, {
+		name: 'Expenses',
+		data: [1100, 3200, 4500, 3200, 3400, 3150, 2100, 2500, 3050, 1920, 2750, 4000]
+	}],
+	options: {
+		chart: {
+			toolbar: {
+				show: false
+			},
+			zoom: {
+				enabled: false
+			}
+		},
+		colors: [
+			'#00d123',
+			'#ff9900'
+		],
+		dataLabels: {
+			enabled: false
+		},
+		stroke: {
+			curve: 'smooth'
+		},
+		legend: {
+			show: false
+		},
+		xaxis: {
+			type: 'datetime',
+			categories: [
+				"2021-01-01",
+				"2021-02-01",
+				"2021-03-01",
+				"2021-04-01",
+				"2021-05-01",
+				"2021-06-01",
+				"2021-07-01",
+				"2021-08-01",
+				"2021-09-01",
+				"2021-10-01",
+				"2021-11-01",
+				"2021-12-01",
+			]
+		},
+		yaxis: {
+			show: false,
+			labels: {
+				formatter: function (value) {
+					return `$${value}`;
+				}
+			},
+		},
+		tooltip: {
+			x: {
+				format: 'dd/MM/yy HH:mm'
+			},
+		},
+	},
+}
+const demochart2 = {
+	series: [
+		{
+			name: 'Savings',
+			data: [
+				{
+					x: 'Jan',
+					y: 1292,
+					goals: [
+						{
+							name: 'Expected',
+							value: 1300,
+							strokeHeight: 5,
+							strokeColor: '#ff9900'
+						}
+					]
+				},
+				{
+					x: 'Feb',
+					y: 2350,
+					goals: [
+						{
+							name: 'Expected',
+							value: 2500,
+							strokeHeight: 5,
+							strokeColor: '#ff9900'
+						}
+					]
+				},
+				{
+					x: 'Mar',
+					y: 3423,
+					goals: [
+						{
+							name: 'Expected',
+							value: 3600,
+							strokeHeight: 5,
+							strokeColor: '#ff9900'
+						}
+					]
+				},
+				{
+					x: 'Apr',
+					y: 3653,
+					goals: [
+						{
+							name: 'Expected',
+							value: 3420,
+							strokeHeight: 5,
+							strokeColor: '#ff9900'
+						}
+					]
+				},
+				{
+					x: 'May',
+					y: 2133,
+					goals: [
+						{
+							name: 'Expected',
+							value: 2150,
+							strokeHeight: 5,
+							strokeColor: '#ff9900'
+						}
+					]
+				},
+				{
+					x: 'Jun',
+					y: 3173,
+					goals: [
+						{
+							name: 'Expected',
+							value: 2000,
+							strokeHeight: 5,
+							strokeColor: '#ff9900'
+						}
+					]
+				},
+				{
+					x: 'Jul',
+					y: 2530,
+					goals: [
+						{
+							name: 'Expected',
+							value: 2115,
+							strokeHeight: 5,
+							strokeColor: '#ff9900'
+						}
+					]
+				},
+				{
+					x: 'Aug',
+					y: 1790,
+					goals: [
+						{
+							name: 'Expected',
+							value: 1800,
+							strokeHeight: 5,
+							strokeColor: '#ff9900'
+						}
+					]
+				},
+				{
+					x: 'Sep',
+					y: 2180,
+					goals: [
+						{
+							name: 'Expected',
+							value: 2000,
+							strokeHeight: 5,
+							strokeColor: '#ff9900'
+						}
+					]
+				},
+				{
+					x: 'Oct',
+					y: 3540,
+					goals: [
+						{
+							name: 'Expected',
+							value: 3650,
+							strokeHeight: 5,
+							strokeColor: '#ff9900'
+						}
+					]
+				},
+				{
+					x: 'Nov',
+					y: 3850,
+					goals: [
+						{
+							name: 'Expected',
+							value: 3600,
+							strokeHeight: 5,
+							strokeColor: '#ff9900'
+						}
+					]
+				},
+				{
+					x: 'Dec',
+					y: 3300,
+					goals: [
+						{
+							name: 'Expected',
+							value: 3125,
+							strokeHeight: 5,
+							strokeColor: '#ff9900'
+						}
+					]
+				},
+			]
+		}
+	],
+	options: {
+		chart: {
+			toolbar: {
+				show: false
+			},
+			zoom: {
+				enabled: false
+			}
+		},
+		plotOptions: {
+			bar: {
+				columnWidth: '60%'
+			}
+		},
+		colors: ['#00d123'],
+		dataLabels: {
+			enabled: false
+		},
+		legend: {
+			show: true,
+			showForSingleSeries: true,
+			customLegendItems: ['Savings', 'Expected'],
+			markers: {
+				fillColors: ['#00d123', '#ff9900']
+			}
+		},
+		yaxis: {
+			show: false,
+			labels: {
+				formatter: function (value) {
+					return `$${value}`;
+				}
+			},
+		},
+	},
+}
 
 const Home = () => {
 	const [anchorElNav, setAnchorElNav] = useState(null)
@@ -85,7 +344,7 @@ const Home = () => {
 											display: { xs: 'block', md: 'none' },
 										}}
 									>
-										{mainMenuList.map((item,index) => (
+										{mainMenuList.map((item, index) => (
 											<MenuItem key={index} component={Link} to={item.to} >
 												<ListItemIcon>
 													{item.icon}
@@ -95,13 +354,13 @@ const Home = () => {
 										))}
 									</Menu>
 								</Box>
-	
+
 								<Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-									{mainMenuList.map((item,index) => (
+									{mainMenuList.map((item, index) => (
 										<Button
 											key={index}
 											component={Link}
-											to={item.to} 
+											to={item.to}
 											sx={{ mx: 3, color: 'white', display: 'flex' }}
 											startIcon={item.icon}
 											size='large'
@@ -148,26 +407,74 @@ const Home = () => {
 
 			<Box>
 				<Container fixed>
-					<Paper elevation={3} sx={{ my: { xs: 4, lg: 8}, p: { xs: 4, lg: 8} }}>
-						<Typography variant="body1" gutterBottom paragraph>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos commodi libero nam? Molestiae, laborum quibusdam ratione cupiditate esse natus est assumenda voluptates? Ducimus nesciunt dolorem, possimus nam excepturi eaque quasi.
-							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus recusandae, quaerat repudiandae dolorem quos, ratione est qui iste inventore esse, et asperiores veritatis reprehenderit minus molestias corporis totam. Asperiores, porro!
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci sed quod debitis impedit ipsum corrupti eligendi, ex blanditiis repellat modi beatae aspernatur sunt, culpa pariatur laudantium est earum? Iusto, modi.
-							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum quaerat delectus itaque, animi quo corrupti nostrum eligendi exercitationem distinctio ipsa numquam odit fugiat porro eum voluptatum quos qui nam deserunt.
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos commodi libero nam? Molestiae, laborum quibusdam ratione cupiditate esse natus est assumenda voluptates? Ducimus nesciunt dolorem, possimus nam excepturi eaque quasi.
-							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus recusandae, quaerat repudiandae dolorem quos, ratione est qui iste inventore esse, et asperiores veritatis reprehenderit minus molestias corporis totam. Asperiores, porro!
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci sed quod debitis impedit ipsum corrupti eligendi, ex blanditiis repellat modi beatae aspernatur sunt, culpa pariatur laudantium est earum? Iusto, modi.
-							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum quaerat delectus itaque, animi quo corrupti nostrum eligendi exercitationem distinctio ipsa numquam odit fugiat porro eum voluptatum quos qui nam deserunt.
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos commodi libero nam? Molestiae, laborum quibusdam ratione cupiditate esse natus est assumenda voluptates? Ducimus nesciunt dolorem, possimus nam excepturi eaque quasi.
-							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus recusandae, quaerat repudiandae dolorem quos, ratione est qui iste inventore esse, et asperiores veritatis reprehenderit minus molestias corporis totam. Asperiores, porro!
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci sed quod debitis impedit ipsum corrupti eligendi, ex blanditiis repellat modi beatae aspernatur sunt, culpa pariatur laudantium est earum? Iusto, modi.
-							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum quaerat delectus itaque, animi quo corrupti nostrum eligendi exercitationem distinctio ipsa numquam odit fugiat porro eum voluptatum quos qui nam deserunt.
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos commodi libero nam? Molestiae, laborum quibusdam ratione cupiditate esse natus est assumenda voluptates? Ducimus nesciunt dolorem, possimus nam excepturi eaque quasi.
-							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus recusandae, quaerat repudiandae dolorem quos, ratione est qui iste inventore esse, et asperiores veritatis reprehenderit minus molestias corporis totam. Asperiores, porro!
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci sed quod debitis impedit ipsum corrupti eligendi, ex blanditiis repellat modi beatae aspernatur sunt, culpa pariatur laudantium est earum? Iusto, modi.
-							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum quaerat delectus itaque, animi quo corrupti nostrum eligendi exercitationem distinctio ipsa numquam odit fugiat porro eum voluptatum quos qui nam deserunt.
-						</Typography>
-					</Paper>
+					<Box mt={15}>
+						<Grid container spacing={4} alignItems="center">
+							<Grid item md={5}>
+								<Typography variant="h3" gutterBottom>
+									Track with ease
+								</Typography>
+								<Typography variant="h5">
+									<strong>Drag&Track</strong> makes it easy to keep track of your incomes and expenses on a daily basis
+								</Typography>
+							</Grid>
+							<Grid item md={7}>
+								<Chart type="area" width={'100%'} height={'100%'} style={{ minHeight: '350px' }} options={demochart.options} series={demochart.series} />
+							</Grid>
+						</Grid>
+					</Box>
+
+					<Box mt={15}>
+						<Grid container spacing={4} alignItems="center">
+							<Grid item md={7}>
+								<Chart type="bar" width={'100%'} height={'100%'} style={{ minHeight: '350px' }} options={demochart2.options} series={demochart2.series} />
+							</Grid>
+							<Grid item md={5}>
+								<Typography variant="h3" gutterBottom>
+									Predictions On The Fly
+								</Typography>
+								<Typography variant="h5">
+									With <strong>Drag&Track</strong> it's easier to plan your future savings and next purchases
+								</Typography>
+							</Grid>
+						</Grid>
+					</Box>
+
+					<Box mt={15} mb={15}>
+						<Grid container spacing={4} alignItems="center">
+							<Grid item md={6}>
+								<Typography variant="h3" gutterBottom>
+									Set & Achieve Goals
+								</Typography>
+								<Typography variant="h5">
+									Are you a man of achievements? With <strong>Drag&Track</strong> it takes just a few seconds to set short or long term goals to keep you motivated
+								</Typography>
+							</Grid>
+							<Grid item md={6}>
+								<Card>
+									<CardMedia
+										component="img"
+										height="140"
+										image={goalimage}
+										alt="green iguana"
+									/>
+									<CardContent>
+										<Typography gutterBottom variant="h5" component="div">
+											Tesla Model S
+										</Typography>
+										<Typography variant="body2" color="text.secondary">
+											You are just <strong>$5750</strong> away from your next car!
+											<br />
+											Based on the last 6 months, you'll complete this Goal in 65 days.
+										</Typography>
+									</CardContent>
+									<CardActions>
+										<Button size="small">Goal Overview</Button>
+										<Button size="small">Manage</Button>
+									</CardActions>
+								</Card>
+							</Grid>
+						</Grid>
+					</Box>
 				</Container>
 			</Box>
 		</>
