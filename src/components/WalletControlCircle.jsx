3e8acc96c31825155fd985ab564d59d572e696fd
Szabadi Miklós <szabadimiklos95@gmail.com>
2022-02-03 14:15:02 +0100

    DND: huge progress, continue with this solution

--- src/components/WalletControlCircle.jsx
+++ src/components/WalletControlCircle.jsx
@@ -1,4 +1,6 @@
 import styled from '@emotion/styled'
+import { useDraggable, useDroppable } from '@dnd-kit/core';
+import { CSS } from '@dnd-kit/utilities';
 
 const Circle = styled.div`
 	border-radius: 50%;
@@ -23,13 +25,61 @@ const Circle = styled.div`
 			}
 		}};
 `
+const DraggablePart = styled.div`
+	display: inline-block;
+`
+const DroppablePart = styled.div`
+	display: inline-block;
+`
 
-const WalletControlCircle = ({ type, text }) => {
+const WalletControlCircle = ({ type, text, ukey }) => {
+	let acceptedTypes = []
+	switch (type) {
+		case "income":	
+			acceptedTypes = []
+			break;
+		case "wallet":
+			acceptedTypes = ['wallet','income']
+			break;
+		case "expense":
+			acceptedTypes = ['wallet']
+			break;
+		default:
+			acceptedTypes = []
+			break;
+	}
+
+	/* Dropzone part */
+		const { isOver, setNodeRef:droppableNodeRef } = useDroppable({
+			id: ukey+"drop",
+			data: {
+				accepts: acceptedTypes,
+			},
+		});
+		const droppableStyle = {
+			backgroundColor: isOver ? 'yellow' : undefined,
+		};
+
+	/* Draggable part */
+		const { attributes, listeners, setNodeRef:draggableNodeRef, transform } = useDraggable({
+			id: ukey+"drag",
+			data: {
+				type: type,
+			},
+		});
+		const draggableStyle = transform ? {
+			transform: CSS.Translate.toString(transform),
+		} : undefined;
+
 	return (
 		<>
-			<Circle type={type}>
-				{text}
-			</Circle>
+			<DroppablePart ref={droppableNodeRef} style={droppableStyle}>
+				<DraggablePart ref={draggableNodeRef} {...listeners} {...attributes} style={draggableStyle}>
+					<Circle type={type}>
+						{text}
+					</Circle>
+				</DraggablePart>
+			</DroppablePart>
 		</>
 	)
 }
--- src/components/WalletControlCore.jsx
+++ src/components/WalletControlCore.jsx
@@ -1,30 +1,68 @@
+import { 
+	DndContext,
+	useDndMonitor,
+	MouseSensor,
+	TouchSensor,
+	useSensor,
+	useSensors
+} from "@dnd-kit/core";
+
 import { Box } from "@mui/material"
 import WalletControlCircle from "./WalletControlCircle";
+import styled from '@emotion/styled'
+
+const DroppableTest = styled.div`
+	width: 100%;
+	height: 300px;
+	border: 2px solid black;
+`
+
+const MComponent = () => {
+	// Monitor drag and drop events that happen on the parent `DndContext` provider
+	useDndMonitor({
+		//onDragStart(event) { console.log("onDragStart",event) },
+		//onDragMove(event) { console.log("onDragMove",event) },
+		onDragOver(event) { console.log("onDragOver Currently over",event.over) },
+		onDragEnd(event) {console.log("onDragEnd Ended over",event.over) },
+		//onDragCancel(event) { console.log("onDragCancel",event) },
+	});
+
+	return <></>
+}
 
 const WalletControlCore = () => {
+	const mouseSensor = useSensor(MouseSensor);
+	const touchSensor = useSensor(TouchSensor);
+
+	const sensors = useSensors(
+		mouseSensor,
+		touchSensor,
+	);
+
 	return (
-		<>
+		<DndContext sensors={sensors}>
+			<MComponent />
 			<Box sx={{ border: '1px solid grey' }}>
 				<Box>
 					<div>Incomes</div>
 					{[...Array(3)].map((item, index) =>
-						<WalletControlCircle type="income" key={`income_${index}`} text="Income"/>
+						<WalletControlCircle type="income" key={`income_${index}`} ukey={`income_${index}`} text="Income"/>
 					)}
 				</Box>
 				<Box>
 					<div>Wallets</div>
 					{[...Array(5)].map((item, index) =>
-						<WalletControlCircle type="wallet" key={`wallet_${index}`} text="Wallet"/>
+						<WalletControlCircle type="wallet" key={`wallet_${index}`} ukey={`wallet_${index}`} text="Wallet"/>
 					)}
 				</Box>
 				<Box>
 					<div>Expenses</div>
 					{[...Array(10)].map((item, index) =>
-						<WalletControlCircle type="expense" key={`expense_${index}`} text="Expense"/>
+						<WalletControlCircle type="expense" key={`expense_${index}`} ukey={`expense_${index}`} text="Expense"/>
 					)}
 				</Box>
 			</Box>
-		</>
+		</DndContext>
 	)
 }
 
